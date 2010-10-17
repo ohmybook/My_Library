a294f8e83714c3215262d2e2b7e2f54a7f6279b9
unknown <jizhh@.(none)>
2010-10-17 15:25:50 +0800

    initialize ML project

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'f6b3d1b57e5e0589a09a2e8a656e5c67'
+end
